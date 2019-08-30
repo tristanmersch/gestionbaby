901d74dc4037d6a98a15169ab3a7d9aa421b85e5
tristanmersch <54647885+tristanmersch@users.noreply.github.com>
2019-08-30 13:37:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-const PORT  = 1024
+const PORT  = process.env.PORT || 8080;
 var fs = require('fs');
 var express = require('express');
 var favicon = require('serve-favicon');
